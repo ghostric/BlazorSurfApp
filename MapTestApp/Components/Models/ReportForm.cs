125b658c2ece9e924d3575c984aa3780a9c7e6d0
TTBOWERZ <ttbowers21@gmail.com>
2025-04-12 16:52:38 -0700

    Start of weather reporting forms

---
+++ MapTestApp/Components/Models/ReportForm.cs
@@ -0,0 +1,12 @@
+﻿namespace MapTestApp.Components.Models
+{
+    public class ReportForm
+    {
+        public DateTime Today { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string WarningType { get; set; }
+        public string Author { get; set; }
+        public string AffectedArea { get; set; }
+    }
+}
---
+++ MapTestApp/Components/Pages/WeatherReportForm.razor.cs
@@ -0,0 +1,11 @@
+﻿using MapTestApp.Components.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MapTestApp.Components.Handler;
+
+namespace MapTestApp.Components.Pages
+{
+    public partial class WeatherReportForm
+    {
+    }
+}
